e00cc58b8a523a1d5702a5cd33b4d1f372f06832
Наташа Неровня <natanerovnja@gmail.com>
2016-08-14 09:49:15 +0300

    fix giff

--- scripts/eat.js
+++ scripts/eat.js
@@ -6,7 +6,7 @@ $(function () {
 		var $calories = $(".food." + event.data.food + " .calories").text();
 		if (+$money >= +$price) {
 			$(".money p").text(+$money-(+$price));
-			$(".cat-img").attr("src","design/img/eat-" + event.data.food + ".gif");
+			$(".cat-img").attr("src","design/img/eat-" + event.data.food + ".gif").attr("style", event.data.attr);
 			setTimeout(function () {
 				$(".cat-img").attr("src","design/img/pusheen.png");
 			}, 1500);
@@ -14,9 +14,10 @@ $(function () {
 		}
 	}
 
-	$(".meat").on("click", {food: "meat"} , feed);
-	$(".pizza").on("click", {food: "pizza"} , feed);
-	$(".hamburguer").on("click", {food: "hamburguer"} , feed);
-	$(".donut").on("click", {food: "donut"} , feed);
-	$(".cupcake").on("click", {food: "cupcake"} , feed);
+	$(".meat").on("click", {food: "meat", attr: "margin: 66px 0 0 345px; width: 265px"} , feed);
+	$(".pizza").on("click", {food: "pizza", attr: "margin: 29px 0 0 300px"} , feed);
+	$(".hamburguer").on("click", {food: "hamburguer", attr: "margin: -14px 0 0 313px"} , feed);
+	$(".donut").on("click", {food: "donut", attr: "margin: 44px 0 0 313px"} , feed);
+	$(".cupcake").on("click", {food: "cupcake", attr: "margin: -6px 0 0 332px; height: 371px"} , feed);
 });
+
